45fd94317cce213a242e11de93b15146267ac23a
Nuria Mohamed Hussein <nuria.hussein@student.moringaschool.com>
2025-04-07 19:17:57 +0300

    commit changes

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count > 0:
+        print(count)
+        count-= 1
+    print("Happy New Year!")
+
+happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = [num ** 2 for num in int_list]
+    return squared_list
+
+
+square_integers([1, 2, 3, 4, 5])
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    number = 1
+    while number <= 100:
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+        number+= 1
+
+fizzbuzz()
+
+
